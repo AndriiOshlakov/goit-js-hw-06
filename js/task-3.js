a7f65eb02b5b742a348f8f3935aea9de021f1270
Andrii <andreyoshlakov18051986@gmail.com>
2025-05-01 11:11:19 +0300

    first add

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,27 @@
-function filterArray(numbers, value) {
-  let newArray = [];
-  for (const number of numbers) {
-    if (number > value) {
-      newArray.push(number);
-    }
+class StringBuilder {
+  #value;
+  constructor(params) {
+    this.#value = params;
   }
-  return newArray;
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
